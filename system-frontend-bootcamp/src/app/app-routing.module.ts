40c137c319c50888bded479869b663ac78036ba0
estebanacgit <estebanacosta994@gmail.com>
2022-12-23 20:32:51 -0300

    Quiero pasar proyecto a MAC

--- system-frontend-bootcamp/src/app/app-routing.module.ts
+++ system-frontend-bootcamp/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AssessComponent } from './pages/admin/assess/assess.component';
 import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
+import { ViewAssessComponent } from './pages/admin/view-assess/view-assess.component';
+import { WelcomeComponent } from './pages/admin/welcome/welcome.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { UserDashboardComponent } from './pages/user/user-dashboard/user-dashboard.component';
 import { AdminGuard } from './services/admin.guard';
@@ -24,11 +28,28 @@ const routes: Routes = [
     component: LoginComponent,
     pathMatch: 'full'
   },
-  {
+  { //localhost.com/admin/profile <-- es como la ruta
   path: 'admin',
   component : DashboardComponent,
-  pathMatch: 'full',
-  canActivate: [AdminGuard]
+  canActivate: [AdminGuard],
+  children: [ //<-- declaro el componente hijo
+    {
+      path:'profile',
+      component: ProfileComponent
+    },
+    {
+      path:'',
+      component: WelcomeComponent
+    },
+    {
+      path:'assess',
+      component: AssessComponent
+    },
+    {
+      path:'view-assess',
+      component: ViewAssessComponent
+    }
+  ]
   },
   {
   path: 'user-dashboard',
--- system-frontend-bootcamp/src/app/app.module.ts
+++ system-frontend-bootcamp/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+
+//acordate de agregarlo abajo en los imports bldo
 import {MatButtonModule} from '@angular/material/button';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
@@ -12,7 +14,10 @@ import {MatSnackBarModule} from '@angular/material/snack-bar';
 import {MatCardModule} from '@angular/material/card';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
-
+import {MatListModule} from '@angular/material/list';
+import {MatSelectModule} from '@angular/material/select';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatTableModule} from '@angular/material/table';
 
 
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -24,6 +29,11 @@ import { HomeComponent } from './pages/home/home.component';
 import { authInterceptorProviders } from './services/auth.interceptors';
 import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
 import { UserDashboardComponent } from './pages/user/user-dashboard/user-dashboard.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { SidebarComponent } from './pages/admin/sidebar/sidebar.component';
+import { WelcomeComponent } from './pages/admin/welcome/welcome.component';
+import { AssessComponent } from './pages/admin/assess/assess.component';
+import { ViewAssessComponent } from './pages/admin/view-assess/view-assess.component';
 
 
 @NgModule({
@@ -34,7 +44,12 @@ import { UserDashboardComponent } from './pages/user/user-dashboard/user-dashboa
     LoginComponent,
     HomeComponent,
     DashboardComponent,
-    UserDashboardComponent
+    UserDashboardComponent,
+    ProfileComponent,
+    SidebarComponent,
+    WelcomeComponent,
+    AssessComponent,
+    ViewAssessComponent
   ],
   imports: [
     BrowserModule,
@@ -48,7 +63,11 @@ import { UserDashboardComponent } from './pages/user/user-dashboard/user-dashboa
     MatSnackBarModule,
     MatCardModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatListModule,
+    MatSelectModule,
+    MatDividerModule,
+    MatTableModule
   ],
   providers: [authInterceptorProviders],
   bootstrap: [AppComponent]
